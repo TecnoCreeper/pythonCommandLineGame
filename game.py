d1a0f082d0bddd24294243f08ea64f016c680274
TecnoCreeper <110969133+TecnoCreeper@users.noreply.github.com>
2022-10-02 20:11:29 +0200

    Add files via upload

--- game.py
+++ game.py
@@ -2,25 +2,33 @@ import random
 import os
 import time
 
+
+
 class Player:
-    def __init__(self, job, health, base_damage, armor, bonus_damage):
+    def __init__(self, job, max_health, health, base_damage, armor, bonus_damage, crit_chance, evasion_chance):
         self.job = job
+        self.max_health = max_health
         self.health = health
         self.base_damage = base_damage
         self.armor = armor
         self.bonus_damage = bonus_damage
+        self.crit_chance = crit_chance
+        self.evasion_chance = evasion_chance
 
 
 class Monster:
-    def __init__(self, element, health, base_damage, armor):
+    def __init__(self, element, health, base_damage, armor, burn, stun):
         self.element = element
         self.health = health
         self.base_damage = base_damage
         self.armor = armor
+        self.burn = burn
+        self.stun = stun
+
 
 
 def menu():
-    print("----------\nWelcome to Dragon Slayer\n")
+    print("----------\nWelcome to Dragon Slayer")
     try:
         h = open("highscore.txt", "r")
     except FileNotFoundError:
@@ -32,98 +40,246 @@ def menu():
     print(f"Highscore: {x}")
     print("Press ENTER to start")
     input()
-    next = False
-    # ask job until valid and set job
-    while (next == False):
-        os.system('cls' if os.name == 'nt' else 'clear')
-        print("Choose your job:\n1 - Warrior\n2 - Thief\n3 - Archer")
-        x = input()
-        if x.isdigit():
-            job_selection = int(x)
-            if (job_selection == 1):
-                p1.job = "Warrior"
-                next = True
-                continue
-            elif (job_selection == 2):
-                p1.job = "Thief"
-                next = True
-                continue
-            elif (job_selection == 3):
-                p1.job = "Archer"
-                next = True
-                continue
-    else:
-        print("----------\nJob selected!\n----------")
+
 
 
 def set_p1_stats():
-    global max_health
-    if (p1.job == "Warrior"):
-        p1.health = 125
+    # ask job until valid and set job
+    job_selection = 0
+    while (job_selection != "1" and job_selection != "2" and job_selection != "3"):
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print("Choose your job:\n1 - Warrior\n2 - Thief\n3 - Archer")
+        job_selection = input()
+    else:
+        if (job_selection == "1"):
+            job_selection = "Warrior"
+        elif (job_selection == "2"):
+            job_selection = "Thief"
+        elif (job_selection == "3"):
+            job_selection = "Archer"
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print("----------\nJob selected!")
+    
+    if (job_selection == "Warrior"):
+        p1.health = 150
         p1.base_damage = 15
-        p1.armor = 15
+        p1.armor = 20
+        p1.crit_chance = 10
+        p1.evasion_chance = 5
     elif (p1.job == "Thief"):
         p1.health = 100
         p1.base_damage = 30
         p1.armor = 5
+        p1.crit_chance = 20
+        p1.evasion_chance = 15
     elif (p1.job == "Archer"):
         p1.health = 150
         p1.base_damage = 15
         p1.armor = 10
-    max_health = p1.health
-    p1.bonus_damage = 0
+        p1.crit_chance = 15
+        p1.evasion_chance = 10
+    p1.max_health = p1.health
+
 
 
 def m1_generation():
+    global BURN_DMG
+    global BURN_CHANCE
+    global burn_turns
+    global STUN_CHANCE
+
     #random monster element
     m_elements = ("fire", "earth", "electro")
     m_element = random.choice(m_elements)
     m1.element = m_element
+
     #scaling random stats
-    m1.health = random.randint(50, 150) * floor // 3
-    m1.base_damage = random.randint(10, 50) * floor // 3
+    m1.health = random.randint(50, 100) * floor // 3
+    m1.base_damage = random.randint(10, 40) * floor // 3
+
     #monster effects
-    a = 0
-    if (m1.element == "Earth"):
-        a = int(5 * floor / 2)
-    m1.armor = a
+    #fire / burn
+    if (m1.element == "fire"):
+        m1.burn = True
+        BURN_DMG = 10
+        BURN_CHANCE = 15
+        burn_turns = 0
+    else:
+        m1.burn = False
+
+    #earth / armor
+    if (m1.element == "earth"):
+        m1.armor = int(floor * 5 / 2)
+    else:
+        m1.armor = 0
+
+    #electro / stun
+    if (m1.element == "electro"):
+        m1.stun = True
+        STUN_CHANCE = 15
+    else:
+        m1.stun = False
+
+
+
+def display_stats():
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print(f"----------\nFloor {floor}\n----------\n")
+    print("A monster appeared!\n")
+    if (m1.armor > 0):
+        print(f"Your stats:\t\t\t{m1.element.title()} monster stats:\nHealth: {p1.health}\t\t\tHealth: {m1.health}\nDamage: {p1.base_damage + p1.bonus_damage}\t\t\tDamage: {m1.base_damage}\nArmor: {p1.armor}\t\t\tArmor: {m1.armor}")
+    else:
+        print(f"Your stats:\t\t\t{m1.element.title()} monster stats:\nHealth: {p1.health}\t\t\tHealth: {m1.health}\nDamage: {p1.base_damage + p1.bonus_damage}\t\t\tDamage: {m1.base_damage}\nArmor: {p1.armor}")
+    if (player_burnt == True):
+        print(f"Burn turns left: {burn_turns}")
+    if (player_stunned == True):
+        print("Stunned!")
+    print("----------")
+
+
+def crit_roll():
+    global player_damage
+    #crit roll
+    CRIT_MULT = 1.5
+    if (random.randint(1,100) <= p1.crit_chance):
+        crit = True
+    else:
+        crit = False
+
+    #calculate damage
+    if (crit == True):
+        player_damage = int((p1.base_damage + p1.bonus_damage) * CRIT_MULT)
+        print("Critical attack!")
+        time.sleep(1)
+    else:
+        player_damage = int(p1.base_damage + p1.bonus_damage)
+
+    print(f"You attack for {player_damage}.")
+    time.sleep(0.5)
+
+
+def evade_roll():
+    global dmg_taken
+    global evade
+    #evade roll
+    if (random.randint(1,100) <= p1.evasion_chance):
+        evade = True
+    else:
+        evade = False
+
+    #if evade dmg_taken = 0
+    if (evade == True):
+        dmg_taken = 0
+        print("You evaded the attack!")
+        time.sleep(1)
+    else:
+        dmg_taken = (m1.base_damage - p1.armor)
+
+
+
+def burn_apply():
+    global player_burnt
+    global burn_turns
+    #check if: - monster can apply burn - player no evade - player not burnt yet
+    if (m1.burn == True and evade == False and player_burnt == False):
+
+        #roll burn chance
+        if (random.randint(1,100) <= BURN_CHANCE):
+            player_burnt = True
+            burn_turns = 3
+            print("You got burnt!")
+            time.sleep(1)
+
+    #apply eventual burn damage
+    if (player_burnt == True):
+        p1.health = p1.health - BURN_DMG
+        burn_turns = burn_turns - 1
+
+        #check if burn status has ended
+        if (burn_turns == 0):
+            player_burnt = False
+            print("You are not burnt anymore!")
+            time.sleep(1)
+
+
+
+def stun_apply():
+    global player_stunned
+    if (m1.stun == True and evade == False and player_stunned == False):
+
+        #roll stun chance
+        if (random.randint(1,100) <= STUN_CHANCE):
+            player_stunned = True
+            print("You got stunned!")
+            time.sleep(1)
 
 
 
 def combat():
     global floor
+    global player_burnt
+    global burn_turns
+    global player_stunned
     next = False
+
     #generate monster
     m1_generation()
+
     #combat loop
-    while (next == False): 
-        os.system('cls' if os.name == 'nt' else 'clear')
-        print(f"----------\nFloor {floor}\n----------\n")
-        print("A monster appeared!\n")
-        print(f"Your stats:\t\t\tMonster stats:\nHealth: {p1.health}\t\t\tHealth: {m1.health}\nDamage: {p1.base_damage + p1.bonus_damage}\t\t\tDamage: {m1.base_damage}\nArmor: {p1.armor}\t\t\tArmor: {m1.armor}")
-        #Implement multiple attacks here (new func, choose you attack, mana / special)
-        print("\n----------\nPress ENTER to attack")
+    while (next == False):
+        #display stats
+        display_stats()
+
+        #TOD0 Implement multiple attacks here (new func, choose you attack, mana / special)
+
+        print("Press ENTER to attack")
         input()
-        #calculate monster health after attack
-        m1.health = m1.health - ((p1.base_damage + p1.bonus_damage) - m1.armor)
+
+        if (player_stunned == False):
+            #attack dmg and critical attack chance
+            crit_roll()
+        
+            #calculate monster health
+            m1.health = m1.health - (player_damage - m1.armor)
+            if (m1.armor > 0):
+                print(f"The monster defended for {m1.armor}.")
+                time.sleep(0.5)
+            print(f"Damage inflicted: {player_damage - m1.armor}.")
+            time.sleep(0.5)
+        else:
+            player_stunned = False
+
         #check if monster is dead
         if (m1.health <= 0):
             next = True
-            print("\n----------\nYou defeated the monster!")
+            player_burnt = False
+            os.system('cls' if os.name == 'nt' else 'clear')
+            print("----------\nYou defeated the monster!")
+            time.sleep(1)
             continue
-        #check if damage taken by player is > 0
-        dmg_taken = (m1.base_damage - p1.armor)
+
+        #evade chance
+        evade_roll()
+
+        #burn
+        burn_apply()
+                
+        #stun
+        stun_apply()
+
+        #apply damage taken
         if (dmg_taken > 0):
             p1.health = p1.health - dmg_taken
+
         #check if player is dead
         if (p1.health <= 0):
             lost()
+
     #when monster is dead
     else:
         os.system('cls' if os.name == 'nt' else 'clear')
         floor = floor + 1
         regen()
-        time.sleep(1)
         bonus_selection()
         combat()
 
@@ -132,25 +288,30 @@ def combat():
 def regen():
     #regenerate health
     p1.health = p1.health + int((p1.health * 0.3))
-    if (p1.health > max_health):
-        p1.health = max_health
+    if (p1.health > p1.max_health):
+        p1.health = p1.max_health
     print("----------\nHealth regenerated!")
+    time.sleep(1)
 
 
 
 def bonus_selection():
-    global max_health
-    #bonuses random extraction
-    bonuses = ["max health", "damage", "armor"]
+    #bonuses pool
+    bonuses = ("max health", "damage", "armor")
+
+    #extraxt 3 random bonuses from pool
     bonuses_choices = random.sample(bonuses, 3)
-    b1 = bonuses_choices[0] #b1, b2, b3 are basically aliases
+
+    #b1, b2, b3 are basically aliases
+    b1 = bonuses_choices[0]
     b2 = bonuses_choices[1]
     b3 = bonuses_choices[2]
-    #select bonus until valid
+
+    #select bonus until valid choice
     next = False
     while(next == False):
         os.system('cls' if os.name == 'nt' else 'clear')
-        print("----------\nChoose a bonus:\n")
+        print("----------\nChoose a bonus:")
         print(f"1 - {b1.title()} bonus\n2 - {b2.title()} bonus\n3 - {b3.title()} bonus")
         x = input()
         if x.isdigit():
@@ -159,8 +320,10 @@ def bonus_selection():
                 next = True
                 continue
     else:
-        print("\n----------\nBonus selected\n----------\n")
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print("----------\nBonus selected")
         time.sleep(1)
+
     #bonus assign
     if (b_selection == 1):
         b_selection = b1
@@ -168,9 +331,10 @@ def bonus_selection():
         b_selection = b2
     elif (b_selection == 3):
         b_selection = b3
-    #bonus adding - probably a better way to do this maybe dictionary (?)
+
+    #bonus adding
     if (b_selection == "max health"):
-        max_health = max_health + 15
+        p1.max_health = p1.max_health + 15
     elif (b_selection == "damage"):
         p1.bonus_damage = p1.bonus_damage + 5
     elif (b_selection == "armor"):
@@ -179,47 +343,52 @@ def bonus_selection():
 
 
 def lost():
-    global dead
+    os.system('cls' if os.name == 'nt' else 'clear')
     print("You died :(")
-    dead = True
-    print(f"You defeated {floor} monsters")
+    time.sleep(1)
+    print(f"----------\nYou defeated {floor} monsters")
+    time.sleep(1)
     #save if highscore
     r = open("highscore.txt", "r")
     x = int(r.readline())
+    r.close()
     if (floor > x):
         h = open("highscore.txt", "w")
         h.write(str(floor))
         h.close()
-        print("New highscore!")
-    print("Press ENTER to restart")
+        print("----------\nNew highscore!")
+        time.sleep(1)
+    print("----------\nPress ENTER to restart")
     input()
     game()
 
 
 
-#game
 def game():
-    # Initialize stuff
+    #Initialize stuff, probably bs but it works :wink:
     os.system('cls' if os.name == 'nt' else 'clear')
     global p1
     global m1
     global floor
-    global max_health
-    global dead
-    p1 = Player("0", 0, 0, 0, 0)
-    m1 = Monster("0", 0, 0, 0)
+    global burn_turns
+    global player_burnt
+    global player_stunned
+    p1 = Player("job", 0, 0, 0, 0, 0, 0, 0)
+    m1 = Monster("job", 0, 0, 0, False, False)
     floor = 1
-    max_health = 0
-    dead = False
+    burn_turns = 0
+    player_burnt = False
+    player_stunned = False
     menu()
-    while (dead == False):
-        set_p1_stats()
-        combat()
+    set_p1_stats()
+    combat()
 
 
-p1 = Player("0", 0, 0, 0, 0)
-m1 = Monster("0", 0, 0, 0)
+
+p1 = Player("job", 0, 0, 0, 0, 0, 0, 0)
+m1 = Monster("job", 0, 0, 0, False, False)
 floor = 1
-max_health = 0
-dead = False
+burn_turns = 0
+player_burnt = False
+player_stunned = False
 game()
